9acde1f87b97f7260770648958ab6a6a3f65f63a
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-03-28 19:59:14 -0300

    audit class creation.

---
+++ src/main/java/br/com/api/vagacerta/model/Audit.java
@@ -0,0 +1,26 @@
+package br.com.api.vagacerta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class Audit {
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "update_at")
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+}
