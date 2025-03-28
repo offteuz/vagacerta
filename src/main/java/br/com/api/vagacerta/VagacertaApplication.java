1311f458c7e2310c58a46acdf70015459edc6196
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-03-28 19:56:30 -0300

    first commit.

---
+++ src/main/java/br/com/api/vagacerta/VagacertaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.vagacerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagacertaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VagacertaApplication.class, args);
+    }
+
+}
